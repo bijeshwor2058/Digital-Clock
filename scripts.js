6aa3560988b34cfc6d12d2dc298768a12f79ea30
Bijeshwor Sharma <bijeshwor2058@gmail.com>
2024-08-11 15:01:31 +0530

    some style is added and heading, to make a colorfull clock

--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
 let hrs = document.getElementById("hrs");
 let min = document.getElementById("min");
 let sec = document.getElementById("sec");
+const ampmEl = document.getElementById("ampm");
 
 setInterval(()=>{
   let curr = new Date();
-
+  let ampm = "AM";
 hrs.innerHTML = (curr.getHours() < 10 ? "0":"") + curr.getHours() - 12;
 min.innerHTML = (curr.getMinutes() < 10 ? "0" : "") + curr.getMinutes();
 sec.innerHTML =  (curr.getSeconds() < 10 ? "0" : "") + curr.getSeconds();
+
+if(curr.getHours() >= 12){
+  ampm = "PM";
+}
+
+hrs.style.color = (curr.getHours() >= 12 && curr.getHours() <= 18)? "lightgreen" : "lightblue";
+
+min.style.color = (curr.getMinutes() % 5 === 0)? "yellow" : "aqua";
+
+sec.style.color = (curr.getSeconds() % 2 === 0)? "black" : "white";
+
+ampmEl.innerHTML = ampm;
 },1000);
